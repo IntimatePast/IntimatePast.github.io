16ab80c8e7dd55302cb7d74d70a13ee588614224
qiqispa <53331785+qiqispa@users.noreply.github.com>
2021-06-20 13:30:18 +0900

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,37 @@
+var selectedItems = []; //이 drawing 파일을 block8에 연결 시킨 후, 병원과 동네는 지워줘야함!
+
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min + 1) + min;
+
+}
+
+function setup() {
+
+  var contentDiv = $("#SizeChartModal");
+  var contentWidth = contentDiv.width();
+  var contentHeight = contentDiv.height();
+
+  $(".item").each(function () {
+    var $image = $(this);
+    var width = $image.outerWidth();
+    var height = $image.outerHeight();
+    var top = getRandomNumber(0, contentWidth - height);
+    var left = getRandomNumber(0, contentHeight - width);
+
+    $(this).css({ top: top, left: left, position: 'absolute' });
+  });
+  // 이미지를 랜덤으로 캔버스에 불러오는 코드
+
+}
+
+function draw() {
+  for (var i = 0; i < selectedItems.length; i++) {
+    var item = selectedItems[i];
+    $("#" + item).css({visibility: "visible"});
+  }
+}
+// 선택된 아이템만 보이게 하는 코드 (처음엔 이미지 전체를 hidden 으로 불러옴)
+
+$(document).ready(function () {
+  setup();
+});
